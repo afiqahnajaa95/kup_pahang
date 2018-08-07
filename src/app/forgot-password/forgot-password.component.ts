71eaa121c3c96043e27d518feb90b96ed751eab7
Nurafiqah Najaa <afiqah@p2digital.com>
2018-08-07 15:26:57 +0800

    adding icons

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { Router, ActivatedRoute } from '@angular/router';
-// import { AlertService, UserService } from '../_services/index';
-// import { AngularFireAuth } from 'angularfire2/auth';
-// import { auth } from 'firebase';
 import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,55 +8,12 @@ import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms'
 })
 
 export class ForgotPasswordComponent {
-  // model: any = {};
-  // loading = false;
   forgotpass: FormGroup;
-  constructor(
-      // private route: ActivatedRoute,
-      // private router: Router,
-      private _formBuilder: FormBuilder){}
-      // private userService: UserService,
-      // private alertService: AlertService,
-      // public firebase: AngularFireAuth) { }
+  constructor(private _formBuilder: FormBuilder){}
 
   ngOnInit(){
     this.forgotpass = this._formBuilder.group({
       email: ['', Validators.required]
     })
   }
-  // register() {
-  //     this.loading = true;
-  //     this.userService.create(this.model)
-  //         .subscribe(
-  //             data => {
-  //                 this.alertService.success('Registration successful', true);
-  //                 this.router.navigate(['/login']);
-  //             },
-  //             error => {
-  //                 this.alertService.error(error);
-  //                 this.loading = false;
-  //             });
-  // }
-  // registerF(){
-  //   console.log("Registering user");
-  //   console.log(this.signup);
-  //   this.firebase.auth.createUserWithEmailAndPassword(this.signup.value.email, this.signup.value.password)
-  //     .then((data) => {
-  //       console.log(data.user);
-  //       console.log("Successful login");
-  //       // alert('Register Successful');
-  //       this.router.navigate(['/register', {id: data.user.uid}]);
-  //     })
-  //     .catch((error) => {
-  //     // Handle Errors here.
-  //     var errorCode = error.code;
-  //     var errorMessage = error.message;
-  //     if (errorCode == 'auth/weak-password') {
-  //       alert('The password is too weak.');
-  //     } else {
-  //       alert(errorMessage);
-  //     }
-  //     console.log(error);
-  //   });
-  // }
 }
